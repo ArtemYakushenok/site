20145c8f8ba6f432c2962ffbf5edbe7d70ce7bc5
ArtemYakushenok <ytyb.mater@gmail.com>
2020-02-24 10:22:11 +0300

    second commit

---
+++ laba01/country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba01
+{
+    class country
+    {
+        private string Name { get; set; }
+        private string government { get; set; }
+        private float area { get; set; }
+    }
+}
